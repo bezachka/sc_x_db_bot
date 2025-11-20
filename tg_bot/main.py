49ace9e55c9712b48168349629924cbfc6f61c90
bezachka <ilyaershov135@gmail.com>
2025-11-20 23:26:29 +0300

    fix

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,9 +1,45 @@
 from requests import get, post
 from dotenv import load_dotenv, find_dotenv
 import os
+from pathlib import Path
+from datetime import datetime
+import json
+import webbrowser
 
-load_dotenv()  # автоматически ищет .env в корне проекта
-print(find_dotenv())  # посмотри, есть ли переменные в окружении
+BASE_DIR = Path(__file__).parent
+load_dotenv(BASE_DIR / "keys.env") 
 
-print(os.getenv("CLIENT_SECRET"))
-print(os.getenv("CLIENT_ID"))
+AUTH_URL = os.getenv("AUTH_URL")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+
+data = {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        "scope" : ""
+    }
+
+response = post(url=AUTH_URL, data=data)
+
+TOKEN = "Bearer " + response.json()["access_token"]
+
+def get_auction_history(region, item_id):
+    dict = {}
+    url = f"https://eapi.stalcraft.net/{region}/auction/{item_id}/history"
+    headers = {"Authorization" : TOKEN}
+    response = get(url=url, headers=headers)
+    for i in response.json()["prices"]:
+        data_str = i["time"]
+        dt_object = datetime.fromisoformat(data_str.replace('Z', '+00:00'))
+        if dict.get(dt_object.strftime("%d.%m.%Y")) == None:
+            dict[dt_object.strftime("%d.%m.%Y")] = [i["price"]]
+        else:
+            dict[dt_object.strftime("%d.%m.%Y")].append(i["price"])
+
+    return dict
+
+dict = get_auction_history(region="ru", item_id="dmjwn")
+for i in dict:
+    print(dict.get(i))
+    
