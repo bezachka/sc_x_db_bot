19dcffffe51acca1605901a3bd7afda0048ea796
bezachka <ilyaershov135@gmail.com>
2025-11-24 21:13:24 +0300

    fix

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,54 +1,27 @@
-from requests import get, post
-from dotenv import load_dotenv, find_dotenv
-import os
-from pathlib import Path
-from datetime import datetime
-import json
-import webbrowser
+from fastapi import FastAPI, Request
+from aiogram import Bot, Dispatcher
+import asyncio
 
-BASE_DIR = Path(__file__).parent
-load_dotenv(BASE_DIR / "keys.env") 
+API_TOKEN = "8457431448:AAGwVBaaxo9oS9U70dPygUwzwuiWOP0eUAo"
+bot = Bot(API_TOKEN)
+dp = Dispatcher()
+app = FastAPI()
 
-AUTH_URL = os.getenv("AUTH_URL")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-CLIENT_ID = os.getenv("CLIENT_ID")
+@app.post("/oauth")
+async def oauth(request: Request):
+    data = await request.json()
+    code = data["code"]
+    chat_id = data["state"]
 
-data = {
-        'grant_type': 'client_credentials',
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        "scope" : ""
-    }
+    # можно обработать code прямо тут
+    print("Получен code:", code)
 
-response = post(url=AUTH_URL, data=data)
+    # уведомить пользователя (если нужно)
+    await bot.send_message(chat_id, f"Код получен ботом!\ncode: {code}")
 
-TOKEN = "Bearer " + response.json()["access_token"]
+    return {"status": "ok"}
 
-def get_auction_history(region, item_id):
-    dict = {}
-    url = f"https://eapi.stalcraft.net/{region}/auction/{item_id}/history"
-    headers = {"Authorization" : TOKEN}
-    response = get(url=url, headers=headers)
-    for i in response.json()["prices"]:
-        data_str = i["time"]
-        dt_object = datetime.fromisoformat(data_str.replace('Z', '+00:00'))
-        if dict.get(dt_object.strftime("%d.%m.%Y")) == None:
-            dict[dt_object.strftime("%d.%m.%Y")] = [i["price"]]
-        else:
-            dict[dt_object.strftime("%d.%m.%Y")].append(i["price"])
-
-    return dict
-
-dict = get_auction_history(region="ru", item_id="dmjwn")
-for i in dict:
-    print(f"{dict} : {dict.get(i)}")
-
-
-def get_auction_active_lots(item_id, region):
-    headers = {"Authorization" : TOKEN}
-    url = f"https://eapi.stalcraft.net/{region}/auction/{item_id}/lots"
-    response1 = get(url, headers=headers)
-    return response1.json()
-
-
-    
+# запуск бота
+@app.on_event("startup")
+async def startup():
+    asyncio.create_task(dp.start_polling(bot))
---
+++ tg_bot/parser.py
@@ -0,0 +1,70 @@
+from requests import get, post
+from dotenv import load_dotenv, find_dotenv
+import os
+from pathlib import Path
+from datetime import datetime
+import json
+import base64
+
+
+BASE_DIR = Path(__file__).parent
+load_dotenv(BASE_DIR / "keys.env") 
+
+AUTH_URL = os.getenv("AUTH_URL")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+
+data = {
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        "scope" : ""
+    }
+
+
+url=f"https://exbo.net/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=&response_type=code"
+
+response = post(url="https://exbo.net/oauth/token", data=data)
+
+print(url)
+
+# TOKEN = "Bearer " + response.json()["access_token"]
+
+# url = f"https://eapi.stalcraft.net/ru/friends/beza"
+# headers = {"Authorization" : TOKEN}
+# response1 = get(url=url, headers=headers)
+# print(response1.json())
+
+
+# url = "https://api.github.com/repos/EXBO-Studio/stalcraft-database/contents/ru/items/weapon/assault_rifle/p63d2.json"
+
+# response_new = get(url)
+# files = response_new.json()
+# content = base64.b64decode(files['content']).decode('utf-8')
+# json_data = json.loads(content)
+# print(json.dumps(json_data, indent=2, ensure_ascii=False))
+
+def get_auction_history(region, item_id):
+    dict = {}
+    url = f"https://eapi.stalcraft.net/{region}/auction/{item_id}/history"
+    headers = {"Authorization" : TOKEN}
+    response = get(url=url, headers=headers)
+    for i in response.json()["prices"]:
+        data_str = i["time"]
+        dt_object = datetime.fromisoformat(data_str.replace('Z', '+00:00'))
+        if dict.get(dt_object.strftime("%d.%m.%Y")) == None:
+            dict[dt_object.strftime("%d.%m.%Y")] = [i["price"]]
+        else:
+            dict[dt_object.strftime("%d.%m.%Y")].append(i["price"])
+
+    return dict
+
+def get_auction_active_lots(item_id, region):
+    headers = {"Authorization" : TOKEN}
+    url = f"https://eapi.stalcraft.net/{region}/auction/{item_id}/lots"
+    response1 = get(url, headers=headers)
+    return response1.json()
+
+
+    
